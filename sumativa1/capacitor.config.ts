7adbe82888ad5196822ed33e7a30d233c9e3110e
GIGANico <142134618+GIGANico@users.noreply.github.com>
2023-12-10 15:56:01 -0300

    Creadas las llaves y todo listo

--- sumativa1/capacitor.config.ts
+++ sumativa1/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'sumativa1',
+  appName: 'PokePedia',
   webDir: 'www',
   server: {
     androidScheme: 'https'
